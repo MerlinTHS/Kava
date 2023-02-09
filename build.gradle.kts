fbecbd1680b3f999cfaa9079fabed03683e2d356
Merlin Eden (THS) <m.eden@ths-bs.de>
2023-02-09 19:03:34 +0100

    Add gradle publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,14 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("multiplatform") version "1.8.0" apply false
     id("com.google.devtools.ksp") version "1.8.0-1.0.8" apply false
+    id("com.vanniktech.maven.publish") version "0.24.0" apply false
 }
 
 allprojects {
-    group = "com.github.merlinths.kava"
-    version = "1.0.3"
+    group = "io.github.merlinths"
+    version = "1.0.0"
 
     repositories {
         mavenCentral()
-    }
-}
-
-subprojects {
-    println("In subprojets block!")
-
-    tasks.withType<KotlinCompile>().configureEach {
-        println("Configuring $name in ${project.name}")
     }
 }
